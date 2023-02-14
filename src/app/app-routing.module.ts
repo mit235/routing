919fd9f8a36303e105f01f2d3ba7c2083a0f8722
mit <meetlimbani5@gmail.com>
2023-02-14 18:39:11 +0530

    routing commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'about',
+  component:AboutComponent,
+  children:[{
+    path:'aboutMe',
+    component:AboutMeComponent
+  },{
+    path:'aboutCompany',
+    component:AboutCompanyComponent
+  }
+]
+},
+{
+  
+  component:UserComponent,
+  path:'user/:id'
+},
+{
+  path:'',
+  component:HomeComponent
+},
+{
+  path:'**',
+  component:NoPageComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NoPageComponent } from './no-page/no-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HomeComponent,
+    AboutComponent,
+    NoPageComponent,
+    AboutMeComponent,
+    AboutCompanyComponent
   ],
   imports: [
     BrowserModule,
